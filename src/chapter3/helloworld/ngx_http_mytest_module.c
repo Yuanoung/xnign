328b73a691d66cf7eb00d54a1e0a2c06a18fe8ca
yuanoung <yuanoung@outlook.com>
2018-01-31 21:40:37 +0800

    ngx_http_mytest_commands gerenlijie

--- src/chapter3/helloworld/ngx_http_mytest_module.c
+++ src/chapter3/helloworld/ngx_http_mytest_module.c
@@ -16,8 +16,8 @@ static ngx_command_t  ngx_http_mytest_commands[] =
     {
         ngx_string("mytest"),
         NGX_HTTP_MAIN_CONF | NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_NOARGS,
-        ngx_http_mytest,
-        NGX_HTTP_LOC_CONF_OFFSET,
+        ngx_http_mytest,  // 滿足配置後，調用的函數
+        NGX_HTTP_LOC_CONF_OFFSET,  // 解析配置
         0,
         NULL
     },
